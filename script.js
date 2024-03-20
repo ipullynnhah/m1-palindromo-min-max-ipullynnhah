e2cd1981a9a202aadea39aa94b1f7ee83c95de68
ipullynnhah <ipullynnhah@gmail.com>
2024-03-20 02:22:09 -0300

    feat: implement isPalindrome function with for loop

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  const SPACE = " ";
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] === SPACE) {
+      i++;
+    }
+    if (str[j] === SPACE) {
+      j--;
+    }
+
+    if (str[i].toLowerCase() !== str[j].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
